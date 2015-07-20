88227ed5c0f039b9555d035757aca794e75fcb69
Dima Shulga <shudima@gmail.com>
2015-07-20 11:23:34 +0300

    add max processes

--- dimas-parallel.js
+++ dimas-parallel.js
@@ -1,34 +1,74 @@
 ﻿var child = require('child_process');
 var Promise = require('promise');
+var os = require('os');
 
 module.exports = {
-    execute : execute
+    execute : execute,
+    maxNumberOfProcesses : maxNumberOfProcesses
 }
 
+var maxNumberOfProcesses = os.cpus().length - 1;
+
+var numberOfRunningProcesses = 0;
+var requestsQueue = [];
 
 function execute(func, args) {
     return new Promise(function(resolve, reject) {
 
+        var request = { func: func, args: args, resolve: resolve, reject: reject };
+        requestsQueue.push(request);
+
+        if (numberOfRunningProcesses < maxNumberOfProcesses) {
+
+            executeFromQueue();
+        }
+    });
+}
+
+function executeFromQueue() {
+    var toExecute = requestsQueue.shift();
+
+    executeRequest(toExecute).then(function (result) {
+        toExecute.resolve(result);
+        executeFromQueue();
+    }).catch(function (error) {
+        toExecute.reject(error);
+        executeFromQueue();
+    });
+}
+
+function executeRequest(request) {
+
+    return new Promise(function(resolve, reject) {
+        var func = request.func;
+        var args = request.args;
+        
         var arguments = getArguments(args);
-        var newConsoleCommand = 'WriteToConsole_' + Math.random().toString().replace('.','');
+        var newConsoleCommand = 'WriteToConsole_' + Math.random().toString().replace('.', '');
         var replaceConsoleCommand = 'var ' + newConsoleCommand + ' = console.log; console.log = function () {};';
         var funcCommand = func.toString().replace(/(\r\n|\n|\r)/gm, "");
         var outputCommand = newConsoleCommand + '(' + func.name + '(' + arguments + '));';
         var command = 'node -e "' + replaceConsoleCommand + ' ' + funcCommand + ' ' + outputCommand;
-
+        
+        console.log('executing' + args);
+        numberOfRunningProcesses++;
         child.exec(command, function (error, stdout, stderr) {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(JSON.parse(stdout.replace(/(\r\n|\n|\r)/gm, "")));
-                }
-            });
+            
+            numberOfRunningProcesses--;
+            
+            if (error) {
+                reject(error);
+            } else {
+                resolve(JSON.parse(stdout.replace(/(\r\n|\n|\r)/gm, "")));
+            }
+        });
     });
 }
 
-
 function getArguments(args) {
-    if (!args) return '';
+    if (args == undefined) return '';
+
+    if (!(args instanceof Array)) args = [args];
 
     var result = [];
 
